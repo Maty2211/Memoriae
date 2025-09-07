b280354fc61894e4c6ea9d05892c965f6ad1eb56
Maty2211 <matiasycortes@gmail.com>
2025-09-06 22:25:19 -0300

    Class  Temporizador

--- pomodoro/models.py
+++ pomodoro/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
-class Pomodoro(models.Model):
+class Temporizador(models.Model):
     #Falta que luego vincule al usuario con el pomodoro para que pueda tener una configuración personalizada.
-    name = models.CharField(max_length=30, default='Configuración predetermianda')
+    title = models.CharField(max_length=100, default='Configuración predetermianda')
+    category = models.CharField(max_length=50)
     #Work y Rest será los tiempos de trabajo y descando (se guardan en minutos).
-    work = models.IntegerField(default=25)
+    hours = models.IntegerField(default=0)
+    minutes = models.IntegerField(default=25)
+    seconds = models.IntegerField(default=0)
     rest = models.IntegerField(default=5)
+    uuid = models.IntegerField(default=0) #A modo de víncular el temporizador con el id de un usuario
 
     def __str__(self):
         return self.name
