8abe002238e025f95a2d1ca3d2e815f3419c6534
Maty2211 <matiasycortes@gmail.com>
2025-09-11 20:45:36 -0300

    atibutos del temporizador

--- pomodoro/models.py
+++ pomodoro/models.py
@@ -1,11 +1,28 @@
 from django.db import models
 
 class Pomodoro(models.Model):
-    #Falta que luego vincule al usuario con el pomodoro para que pueda tener una configuración personalizada.
-    name = models.CharField(max_length=30, default='Configuración predetermianda')
-    #Work y Rest será los tiempos de trabajo y descando (se guardan en minutos).
-    work = models.IntegerField(default=25)
-    rest = models.IntegerField(default=5)
+    title = models.CharField(max_length=30, default='Configuración predetermianda')
+    working_time = models.IntegerField(default=25) #Tiempo de trabajo en minutos
+    break_time = models.IntegerField(default=5) #Tiempo de descanso en minutos
+    sessions = models.IntegerField(default=0) #Número de sesiones (pomodoro) completadas (en minutos)
+
+    def add_working_time(self):
+        self.working_time += 5
+        self.save()
+    
+    def subtract_working_time(self):
+        if self.working_time > 5:
+            self.working_time -= 5
+            self.save()
+    
+    def add_break_time(self):
+        self.break_time += 5
+        self.save()
+    
+    def subtract_break_time(self):
+        if self.break_time > 5:
+            self.break_time -= 5
+            self.save()
 
     def __str__(self):
-        return self.name
+        return self.title
--- pomodoro/views.py
+++ pomodoro/views.py
@@ -1,4 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
+from . models import Pomodoro
 
 def home(request):
     return render(request, 'index.html', {})
+
+def create_pomodoro(request):
+    if request.method == 'POST':
+        # Crea un nuevo Pomodoro con los valores por defecto 
+        new_pomodoro = Pomodoro.objects.create(user=request.user)   #Paso el usuario actual como parámetrp
+        return redirect('pomodoro_detail', pk=new_pomodoro.pk) # Redirige al detalle del nuevo objeto
+    return render(request, 'pomodoro_widget.html', {})
+
+def pomodoro_detail(request, pk):
+    pomodoro = get_object_or_404(Pomodoro, pk=pk)
+    return render(request, 'pomodoro_detail.html', {'pomodoro': pomodoro})
+
+def add_working_time_view(request, pk):
+    pomodoro = get_object_or_404(Pomodoro, pk=pk, user=request.user)
+    pomodoro.add_working_time()
+    return HttpResponse(pomodoro.working_time)
+
+def subtract_working_time_view(request, pk):
+    pomodoro = get_object_or_404(Pomodoro, pk=pk, user=request.user)
+    pomodoro.subtract_working_time()
+    return HttpResponse(pomodoro.working_time)
+
+def add_break_time_view(request, pk):
+    pomodoro = get_object_or_404(Pomodoro, pk=pk, user=request.user)
+    pomodoro.add_break_time()
+    return HttpResponse(pomodoro.break_time)
+
+def subtract_break_time_view(request, pk):
+    pomodoro = get_object_or_404(Pomodoro, pk=pk, user=request.user)
+    pomodoro.subtract_break_time()
+    return HttpResponse(pomodoro.break_time)
