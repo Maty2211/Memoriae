0c0abdb63e16bf558ab56459428e4e7f1baa91e0
itsiaraaar <businessiara47@gmail.com>
2025-09-28 00:52:59 -0300

    probando cosas

--- backend/memoriae/settings.py
+++ backend/memoriae/settings.py
@@ -10,12 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-from decouple import config
+from decouple import Config, RepositoryEnv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+ENV_FILE = BASE_DIR.parent / '.env'     # <-- Memoriae/.env
+config = Config(RepositoryEnv(ENV_FILE))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
