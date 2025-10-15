a9d1e13cbde0a1b8e4d5a8fd2bdb8c5e05c68cc6
Maty2211 <matiasycortes@gmail.com>
2025-10-15 01:53:49 -0300

    Ajuste front pomodoro + carrusel background

--- backend/apps/calendario/urls.py
+++ backend/apps/calendario/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from . import views
+from calendario import views
 
 router = routers.DefaultRouter()
 router.register(r'calendario', views.EventView, 'calendario')
 
 urlpatterns = [
+    path('api/v1/', include(router.urls))
     path('api/v1/', include(router.urls))
 ]
