6e2bd121ab0764560b5c671b0bdaf53bb82b6fee
itsiaraaar <businessiara47@gmail.com>
2025-10-15 17:05:27 -0300

    login bonito
    :wq
    
    :wq

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,6 +11,8 @@ export default defineConfig({
       "/calendario": { target: "http://localhost:8000", changeOrigin: true },
       "/to_do_list": { target: "http://localhost:8000", changeOrigin: true },
       "/flashcard": { target: "http://localhost:8000", changeOrigin: true },
+      "/pomodoro": {target: 'http://localhost:8000', changeOrigin: true,
+      },
       // agrega otras rutas (evento, to do, etc.)
     },
   },
