d679547ef90dd47e18b832a7174f3280f85b9637
Maty2211 <matiasycortes@gmail.com>
2025-10-16 02:43:51 -0300

    Quita de alertas

--- backend/apps/pomodoro/serializers.py
+++ backend/apps/pomodoro/serializers.py
@@ -2,22 +2,17 @@ from rest_framework import serializers
 from .models import PomodoroSettings, PomodoroHistory
 
 class PomodoroSettingsSerializer(serializers.ModelSerializer):
-    """Serializador para la configuración de tiempo del Pomodoro."""
-    
     class Meta:
         model = PomodoroSettings
-        # Excluimos 'user' porque lo gestionaremos automáticamente en la vista.
         fields = ['id', 'title', 'work_time', 'break_time', 
                   'long_break_time', 'sessions_completed', 
                   'sessions_until_long_break']
-        read_only_fields = ['sessions_completed'] # El frontend no debe modificar el contador directamente.
+        read_only_fields = ['sessions_completed']
 
 
-class PomodoroHistorySerializer(serializers.ModelSerializer):
-    """Serializador para registrar sesiones de Pomodoro completadas."""
-    
+class PomodoroHistorySerializer(serializers.ModelSerializer):  
     class Meta:
         model = PomodoroHistory
         fields = ['id', 'session_type', 'start_time', 'end_time', 
                   'duration_minutes', 'was_successful']
-        read_only_fields = ['duration_minutes'] # Se calcula en la vista antes de guardar.
+        read_only_fields = ['duration_minutes']
--- backend/apps/pomodoro/urls.py
+++ backend/apps/pomodoro/urls.py
@@ -2,12 +2,10 @@ from django.urls import path
 from .views import PomodoroSettingsView, PomodoroSessionAPIView, PomodoroHistoryListView
 
 urlpatterns = [
-    # GET/PUT/PATCH: Para obtener y actualizar la configuración del Pomodoro
+    #GET y PUT
     path('api/v1/settings/', PomodoroSettingsView.as_view(), name='pomodoro-settings'),
-    
-    # POST: Para registrar una sesión completada (Focus, Short/Long Break)
+    # POST
     path('api/v1/sessions/complete/', PomodoroSessionAPIView.as_view(), name='pomodoro-session-complete'),
-
-    # GET: Para obtener la lista de sesiones del historial
+    # GET
     path('history/', PomodoroHistoryListView.as_view(), name='pomodoro-history'),
 ]
