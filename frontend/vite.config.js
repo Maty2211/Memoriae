8b635eaebc22ec24ae1adc2c8533346d37c20c2b
Maty2211 <matiasycortes@gmail.com>
2025-10-16 07:48:59 -0300

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
       host: '0.0.0.0',
       port: 8080,
       strictPort: true,
-      allowedHosts: ['memoriae-web-production.up.railway.app']
+      allowedHosts: true,
     }
   },
 });
