58526f23b06395edd86a3a3316aa9937394d9dd0
patricia <patriciavalentinazacariasfuentes@alu.frlp.utn.edu.ar>
2025-10-16 08:19:16 -0300

    help

--- frontend/src/api/perfil.api.js
+++ frontend/src/api/perfil.api.js
@@ -1,13 +1,19 @@
+// en perfil.api.js
+
 const BASE_URL = "http://127.0.0.1:8000/api/perfil";
 
 export const obtenerEstadisticasPerfil = async () => {
   try {
+    // Ya no necesitas leer de localStorage
     const res = await fetch(`${BASE_URL}/stats/`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
       },
+      // --- ¡AÑADE ESTA LÍNEA! ---
+      credentials: 'include',
     });
+    
     if (!res.ok) throw new Error("Error al obtener estadísticas del perfil");
     return await res.json();
   } catch (err) {
