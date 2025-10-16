ce93e2e43852d8246f3fb18b83e822b3f59b0727
Maty2211 <matiasycortes@gmail.com>
2025-10-16 09:47:02 -0300

    Cambio de urls

--- backend/memoriae/settings.py
+++ backend/memoriae/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'memoriae.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-DATABASES = {
+"""DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": config("DB_NAME"),
