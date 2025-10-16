2cc81cb4bc481670c9ea635fbcdc5e99685bdd95
Maty2211 <matiasycortes@gmail.com>
2025-10-16 08:01:29 -0300

    Pomodoro new url

--- frontend/src/api/pomodoro.api.js
+++ frontend/src/api/pomodoro.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const pomodoroApi = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://memoriae-api-production.up.railway.app/",
   withCredentials: true,
 });
 
